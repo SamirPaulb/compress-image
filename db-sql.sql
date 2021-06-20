157058d78523b4f409e44e275c69d1b738d5df42
SAMIR PAUL <77569653+SamirPaul1@users.noreply.github.com>
2021-06-20 18:30:23 +0530

    Add files via upload

---
+++ db-sql.sql
@@ -0,0 +1,42 @@
+
+--
+-- Database: `db_contact`
+--
+
+CREATE DATABASE IF NOT EXISTS `db_contact` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `db_contact`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_contact`
+--
+
+DROP TABLE IF EXISTS `tbl_contact`;
+CREATE TABLE `tbl_contact` (
+  `id` int(11) NOT NULL,
+  `fldName` varchar(50) NOT NULL,
+  `fldEmail` varchar(150) NOT NULL,
+  `fldPhone` varchar(15) NOT NULL,
+  `fldMessage` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
